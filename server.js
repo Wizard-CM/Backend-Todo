2bba4b027747d686e37de44bf0493b8c35355888
Ronak <ronakthapa37@gmail.com>
2023-12-12 16:02:46 +0545

    f

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import { app } from "./app.js";
 
-let port = process.env.PORT || 4000;
-app.listen(port, () => {
+// let port = process.env.PORT || 4000;
+
+app.listen(process.env.PORT, () => {
   console.log(`server Running on Port ${process.env.PORT} in ${process.env.NODE_ENV} server`);
 });
 
